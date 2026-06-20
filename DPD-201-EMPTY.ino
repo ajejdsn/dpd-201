80804686e419d50b3a23b64cf20ae279f98400b3
Sn Ajejd <165407102+ajejdsn@users.noreply.github.com>
2026-06-20 20:56:35 +0300

    Update DPD-201-EMPTY.ino

--- DPD-201-EMPTY.ino
+++ DPD-201-EMPTY.ino
@@ -2,48 +2,50 @@
 #define LTX 2 
 #define LRX -1 
 // we don't need RX ^
-SoftwareSerial lcd(LRX, LTX);
+SoftwareSerial vfd(LRX, LTX);
 // EXAMPLE w/o TESTS
 // https://github.com/ajejdsn/dpd-201
 // =D
 
 void setup()
 {
-  lcd.begin(9600);
-  Serial.begin(9600);
-  lcd.write(231); // CLS
-  delay(300);
+  vfd.begin(9600);
   rein();
-  Serial.println(F("READY!!!"));
 }
 void loop() {
-  //...nothing (¬ω⌐)
-  // uh.. add ur code here?
+  
 }
 
 void printA(byte x) {
-    lcd.write(eD(x));   // print ASCII (ex. A=65)
+    vfd.write(eD(x));   // print ASCII (ex. A=65)
 }
 
 void rein() // clear screen
 {
-  lcd.write(231); // CLS
+  vfd.write(231); // CLS
   delay(50);
 } 
 
 inline uint8_t eD(uint8_t x) {
   asm volatile (
     "LSL %0 \n\t" // ROL
-    "ADC %0,__zero_reg__ \n\t" // x+0 + carry... x.x
+    "ADC %0,__zero_reg__ \n\t" // x+0 + carry... wtf...
     "COM %0 \n\t" // NOT (~)
     :"+r" (x) // return
     );
     return x;
 }
 
-void printR(const char* text) {
-  while (*text) {
-    lcd.write(eD((uint8_t)*text)); // write ASCII text
-    text++;
+void printR(const char* t) {
+  while (*t) {
+    vfd.write(eD((uint8_t)*t)); // write ASCII text
+    t++;
   }
 }
+
+/* 
+ *  2412/30720 bytes of ROM
+ *  126/2048 bytes of RAM.
+ *  If I'll use hardware serial, it'll be better.
+ *  Although, it's already looks good.
+ */
